35fdb5d532cafc83bf03499085ddd24b7a19c2dd
Luke Rabbitte <lrabbitt@tcd.ie>
2022-11-28 16:10:30 +0100

    Montrer les valeurs dans chaque nouveau table

--- actions/v1_action_tablesData.py
+++ actions/v1_action_tablesData.py
@@ -37,9 +37,10 @@ class AppTablesDataV1(QDialog):
     # Fonction permettant de mettre à jour toutes les tables
     @pyqtSlot()
     def refreshAllTablesV1(self):
-
         self.refreshTable(self.ui.label_epreuves, self.ui.tableEpreuves, "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, dateEp FROM LesEpreuves")
         self.refreshTable(self.ui.label_sportifs, self.ui.tableSportifs, "SELECT numSp, nomSp, prenomSp, pays, categorieSp, dateNaisSp, numEq FROM LesSportifsEQ")
+        self.refreshTable(self.ui.labelInscriptions, self.ui.tableInscriptions, "SELECT numIn, numEp from LesInscriptions")
+        self.refreshTable(self.ui.labelResultats, self.ui.tableResultats, "SELECT numEp, gold, silver, bronze from LesResultats")
 
         # TODO 1.3 : modifier pour afficher les nouveaux éléments (il faut aussi changer le fichier .ui correspondant)
         # TODO 1.4b : ajouter l'affichage des éléments de la vue LesAgesSportifs après l'avoir créée
