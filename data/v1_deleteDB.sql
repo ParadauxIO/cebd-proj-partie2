f3a251ad16a6d1bcc1e6ef8bb3fcb8f358d6f19b
Rían Errity <rian@paradaux.io>
2022-11-28 15:32:52 +0100

    Lire d'excel pour LesInscriptions / LesResultats

--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS LesEpreuves;
 DROP TABLE IF EXISTS LesSportifsEQ;
 DROP TABLE IF EXISTS LesInscriptions;
 DROP TABLE IF EXISTS LesResultats;
+
 DROP VIEW IF EXISTS LesAgesSportifs;
 DROP VIEW IF EXISTS LesNbEquipiers;
 -- TODO 3.3 : pensez à détruire vos triggers !
